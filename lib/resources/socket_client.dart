bb0d267cd3bc8935bd294b236a206f4a63d8576f
Nitayasit Chatreewudtichai <nitayasit.cha@student.mahidol.ac.th>
2023-12-17 21:27:35 +0700

    first commit

---
+++ lib/resources/socket_client.dart
@@ -0,0 +1,20 @@
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class SocketClient {
+  IO.Socket? socket;
+  static SocketClient? _instance;
+
+  SocketClient._internal() {
+    //In address select the ip address of the computer that run the codes.
+    socket = IO.io('http://192.168.1.47:3000', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    socket!.connect();
+  }
+
+  static SocketClient get instance {
+    _instance ??= SocketClient._internal();
+    return _instance!;
+  }
+}
